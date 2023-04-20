c7f71b428455c4bd77935fbc845f029e87a2e74c
Marvin Campos <68110881+Marvin-Campos@users.noreply.github.com>
2023-04-21 00:26:04 +0800

    Main Menu Elements

--- app/src/main/java/com/example/interactivenavigationrtumap/MainMenu.java
+++ app/src/main/java/com/example/interactivenavigationrtumap/MainMenu.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("test");
+        getSupportActionBar().hide();
+        setContentView(R.layout.main_menu);
     }
 }
