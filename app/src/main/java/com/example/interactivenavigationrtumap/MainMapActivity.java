e48c17388be7cafdd5a2b75e306685c171a1bfeb
Marvin Campos <68110881+Marvin-Campos@users.noreply.github.com>
2023-05-02 04:38:46 +0800

    Dropdowns on progress

--- app/src/main/java/com/example/interactivenavigationrtumap/MainMapActivity.java
+++ app/src/main/java/com/example/interactivenavigationrtumap/MainMapActivity.java
@@ -3,13 +3,22 @@ package com.example.interactivenavigationrtumap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainMapActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main_map);
+
+        String[] buildings = { "Select Building", "Building 1", "Building 2", "Building 3" };
+        Spinner selectBuildingSpinner = (Spinner) findViewById(R.id.selectbuilding);
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, buildings);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectBuildingSpinner.setAdapter(adapter);
     }
 }
